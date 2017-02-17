6a44c9be752986ff6d2b6e571f7a12776b6f2e8f
Sathes Balasingam <sathes352@gmail.com>
2017-02-17 15:41:06 -0500

    done

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+For the method and status problems they were both GET and 200
+
+For the internet loaded one  the type is xhr and initiator is jquery.min.js:4
+
+For the one I loaded the type is document and the initiator other
+
+XHR finished loading: GET "http://first-ajax-api.herokuapp.com/".
+
+Content of response
+Wow what a ride! I came all the way from the server! 
+
+A internal error occured when trying to load /pong
+XHR finished loading: GET "http://first-ajax-api.herokuapp.com/pong".
--- ajax.js
+++ ajax.js
@@ -1,5 +1,55 @@
 $(document).ready(function () {
 
-  /* Your code goes here */
+$('#step12').on('click', function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    data: {},
+    dataType: 'text'
+  }).done(function (responseData) {
+  })
+})
+$('#step3456').on('click', function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/pong',
+    method: 'GET',
+}).done(function (responseData) {
+  $('#step3456').append(responseData)
+  console.log('Wow what a ride! I came all the way from the server!')
+}).fail(function () {
+  $('#step3456').append('I will try hard next time buddy!!')
+}).always(function () {
+  console.log('Your request is finished buddy!!')
+})
+})
+$('#step7').on('click', function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/count',
+    method: 'GET'
+  }).done(function (responseData){
+    $('#step7').append(responseData)
+  })
+})
+$('#step8').on('click', function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/time',
+    method: 'GET',
+    data: {timezone: 'Pacific/Honolulu'}
+  }).done(function (responseData) {
+  $('#step8').append(responseData)
+})
 
-});
+})
+$('#step9').on('click', function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/a_car',
+    method: 'GET'
+  }).done(function (responseData) {
+    $('#emptyunorderedlist').append(responseData)
+  })
+
+})
+
+
+
+})
